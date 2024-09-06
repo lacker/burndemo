4be1269b65e4c17e018286c03a89f45c7fb0f0ac
Kevin Lacker <lacker@gmail.com>
2024-09-06 15:23:44 -0700

    forward

--- src/model.rs
+++ src/model.rs
@@ -18,6 +18,32 @@ pub struct Model<B: Backend> {
     activation: Relu,
 }
 
+impl<B: Backend> Model<B> {
+    /// # Shapes
+    ///   - Images [batch_size, height, width]
+    ///   - Output [batch_size, num_classes]
+    pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
+        let [batch_size, height, width] = images.dims();
+
+        // Create a channel at the second dimension.
+        let x = images.reshape([batch_size, 1, height, width]);
+
+        let x = self.conv1.forward(x); // [batch_size, 8, _, _]
+        let x = self.dropout.forward(x);
+        let x = self.conv2.forward(x); // [batch_size, 16, _, _]
+        let x = self.dropout.forward(x);
+        let x = self.activation.forward(x);
+
+        let x = self.pool.forward(x); // [batch_size, 16, 8, 8]
+        let x = x.reshape([batch_size, 16 * 8 * 8]);
+        let x = self.linear1.forward(x);
+        let x = self.dropout.forward(x);
+        let x = self.activation.forward(x);
+
+        self.linear2.forward(x) // [batch_size, num_classes]
+    }
+}
+
 #[derive(Config, Debug)]
 pub struct ModelConfig {
     num_classes: usize,
