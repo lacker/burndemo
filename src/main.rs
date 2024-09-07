94084580ac15e3575bbb2ac1a7f8c18c2df48518
Kevin Lacker <lacker@gmail.com>
2024-09-07 09:58:10 -0700

    autodiff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use burn::backend::Wgpu;
+use burn::backend::{Autodiff, Wgpu};
+use burn::optim::AdamConfig;
 use model::ModelConfig;
+use training::TrainingConfig;
 
 mod data;
 mod model;
@@ -7,9 +9,13 @@ mod training;
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{}", model);
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    crate::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
 }
