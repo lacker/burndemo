988474d51a5c283157761735b73b14650b5f3f62
Kevin Lacker <lacker@gmail.com>
2024-09-09 11:37:59 -0700

    inference

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use burn::backend::{Autodiff, Wgpu};
+use burn::data::dataset::Dataset;
 use burn::optim::AdamConfig;
 use model::ModelConfig;
 use training::TrainingConfig;
 
 mod data;
+mod inference;
 mod model;
 mod training;
 
@@ -17,5 +19,13 @@ fn main() {
         artifact_dir,
         TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
         device.clone(),
+    );
+
+    crate::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(42)
+            .unwrap(),
     );
 }
